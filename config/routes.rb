d4ee4a6437cf99b893aa396e6071447a53ba88ec
Basia Madej <basia@applicake.com>
2012-10-02 22:06:21 +0200

    active admin files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Webmuses::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :muses, only: [:index]
   match '/wydarzenia', :to  => 'static_pages#events'
   match '/materialy', :to  => 'static_pages#materials'
