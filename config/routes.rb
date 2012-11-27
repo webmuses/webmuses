46c23841caa20b75bd1e71683948b5a8783ce98e
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2012-11-27 16:32:21 +0100

    newsletter icon

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Webmuses::Application.routes.draw do
+NEWSLETTER_URL = "http://webmus.us5.list-manage1.com/subscribe?u=5e7d2eada5060b03682193094&id=cdb7797b58"
 
+Webmuses::Application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
 
@@ -8,5 +9,6 @@ Webmuses::Application.routes.draw do
   resources :coaches, only: [:index]
   match '/wydarzenia', :to  => 'events#summary'
   match '/materialy', :to  => 'static_pages#materials'
+  match '/newsletter' => redirect(NEWSLETTER_URL)
   root :to  => 'static_pages#home', as: 'home'
 end
