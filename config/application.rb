71d6838a30a21ffd47630d0a1ee03c9b70eee656
Basia Madej <basia@applicake.com>
2013-01-13 22:39:30 +0100

    admin panel css are back

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,7 @@ module Webmuses
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += %w( active_admin.css)
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
