9a45a58ec555264bacddf0bc1bf739b4453ccb5b
Basia Madej <barbara.madej@gmail.com>
2013-06-17 20:22:32 +0200

    fixed twitter and hackerspace logo

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,14 @@
 ActiveRecord::Schema.define(:version => 20130317150752) do
 
   create_table "active_admin_comments", :force => true do |t|
-    t.string    "resource_id",   :null => false
-    t.string    "resource_type", :null => false
-    t.integer   "author_id"
-    t.string    "author_type"
-    t.text      "body"
-    t.timestamp "created_at",    :null => false
-    t.timestamp "updated_at",    :null => false
-    t.string    "namespace"
+    t.string   "resource_id",   :null => false
+    t.string   "resource_type", :null => false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.text     "body"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "namespace"
   end
 
   add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
@@ -29,59 +29,59 @@ ActiveRecord::Schema.define(:version => 20130317150752) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
 
   create_table "admin_users", :force => true do |t|
-    t.string    "email",                  :default => "", :null => false
-    t.string    "encrypted_password",     :default => "", :null => false
-    t.string    "reset_password_token"
-    t.timestamp "reset_password_sent_at"
-    t.timestamp "remember_created_at"
-    t.integer   "sign_in_count",          :default => 0
-    t.timestamp "current_sign_in_at"
-    t.timestamp "last_sign_in_at"
-    t.string    "current_sign_in_ip"
-    t.string    "last_sign_in_ip"
-    t.timestamp "created_at",                             :null => false
-    t.timestamp "updated_at",                             :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "coaches", :force => true do |t|
-    t.text      "description"
-    t.string    "name"
-    t.string    "photo"
-    t.boolean   "visible"
-    t.string    "twitter"
-    t.string    "url"
-    t.timestamp "created_at",  :null => false
-    t.timestamp "updated_at",  :null => false
+    t.text     "description"
+    t.string   "name"
+    t.string   "photo"
+    t.boolean  "visible"
+    t.string   "twitter"
+    t.string   "url"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "events", :force => true do |t|
-    t.string    "name"
-    t.string    "headline"
-    t.text      "description"
-    t.string    "location"
-    t.timestamp "start_at"
-    t.timestamp "end_at"
-    t.string    "fb_url"
-    t.string    "registration_url"
-    t.timestamp "created_at",                            :null => false
-    t.timestamp "updated_at",                            :null => false
-    t.string    "date_description"
-    t.string    "price"
-    t.string    "state",            :default => "draft"
+    t.string   "name"
+    t.string   "headline"
+    t.text     "description"
+    t.string   "location"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.string   "fb_url"
+    t.string   "registration_url"
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
+    t.string   "date_description"
+    t.string   "price"
+    t.string   "state",            :default => "draft"
   end
 
   create_table "muses", :force => true do |t|
-    t.string    "name"
-    t.text      "description"
-    t.boolean   "visible"
-    t.string    "photo_url"
-    t.timestamp "created_at",  :null => false
-    t.timestamp "updated_at",  :null => false
-    t.string    "title"
-    t.string    "twitter"
+    t.string   "name"
+    t.text     "description"
+    t.boolean  "visible"
+    t.string   "photo_url"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.string   "title"
+    t.string   "twitter"
   end
 
   create_table "partners", :force => true do |t|
