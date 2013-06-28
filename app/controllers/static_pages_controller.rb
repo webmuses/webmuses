6af473e55309e27bb1f89bb3de02a82b444b0c1e
Basia Madej <barbara.madej@gmail.com>
2013-06-28 11:44:11 +0200

    flags allowing to switch to between langauges

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,9 @@ class StaticPagesController < ApplicationController
   def home
     @title = "Home"
     @current_events = Event.current.all
-    @past_events = Event.past.limit(2).order("random()")
-    @sponsors = Partner.visible.sponsor.order("position ASC").all
-    @partners = Partner.visible.normal.order("position ASC").all
+    @past_events = Event.past.limit(3).order("random()")
+    @sponsors = Partner.visible.sponsor.all
+    @partners = Partner.visible.normal.all
   end
 
   def materials
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -4,6 +4,7 @@ class Partner < ActiveRecord::Base
   scope :visible, where(visible: true)
   scope :sponsor, where(category: 'sponsor')
   scope :normal, where(category: 'normal')
+  default_scope order("position ASC")
 
   validates :name, uniqueness: true
 end
