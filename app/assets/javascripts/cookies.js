6e9389829ebaffdac1f1704ab46fcd4cd8cb0ad9
Przemysław Dąbek <przemek.dabek@getbase.com>
2013-09-29 11:10:02 +0200

    info about cookies

---
+++ app/assets/javascripts/cookies.js
@@ -0,0 +1,36 @@
+// adapted from: http://webhelp.pl/artykuly/okienko-z-informacja-o-ciasteczkach-cookies/
+
+(function(){
+  function WHCreateCookie(name, value, days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    var expires = "; expires=" + date.toGMTString();
+    document.cookie = name+"="+value+expires+"; path=/";
+  }
+
+  function WHReadCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  window.onload = WHCheckCookies;
+
+  function WHCheckCookies() {
+    if(WHReadCookie('cookies_accepted') == 'true') {
+      WHCloseCookiesWindow();
+    }
+  }
+
+  function WHCloseCookiesWindow() {
+    WHCreateCookie('cookies_accepted', true, 365);
+    $('#cookies-message-container').remove();
+  }
+
+  $('#accept-cookies').on('click', WHCloseCookiesWindow);
+})();
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_locale
+  before_filter :set_cookies_unaccepted
 
   def set_locale
     default = admin_area? ? :en : I18n.default_locale
     I18n.locale = params[:locale] || default
+  end
+
+  def set_cookies_unaccepted
+    @cookies_unaccepted = (cookies['cookies_accepted'] != 'true')
   end
 
   def default_url_options(options={})
