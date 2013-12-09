32060a58fc81a1469cc8dc745b68e7df36c69638
Basia Madej <barbara.madej@gmail.com>
2013-12-09 02:21:04 +0100

    make jQuery work with turbolinks

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,10 @@
-$(document).ready(function() {
+$(document).on("ready page:load", function() {
   $('nav ul li a:not(.logo)').each(function() {
    if (jQuery(this).attr('href')  ===  window.location.pathname) {
      jQuery(this).addClass('active');
    }});
   $("ul#tabs").tabs("div.tabs_holder > div", {tabs: 'li'});
+
   document.createElement("article");
   document.createElement("aside");
   document.createElement("footer");
