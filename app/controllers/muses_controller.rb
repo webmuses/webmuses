002d2ad2583e43507bc5519038c50e8c282aed4a
Basia Madej <barbara.madej@gmail.com>
2017-01-06 20:38:35 +0100

    Gosia and exMuses

--- app/controllers/muses_controller.rb
+++ app/controllers/muses_controller.rb
@@ -3,5 +3,6 @@ class MusesController < ApplicationController
   def index
     @title = t('muses.headline')
     @muses = Muse.visible.all.shuffle
+    @ex_muses = Muse.invisible.all.shuffle
   end
 end
--- app/models/muse.rb
+++ app/models/muse.rb
@@ -1,5 +1,6 @@
 class Muse < ActiveRecord::Base
   include I18nable
   scope :visible, -> { where(visible: true) }
+  scope :invisible, -> { where(visible: false) }
   validates :name, :uniqueness => {:allow_blank => true}, :presence => true
 end
