64675f81197b2a74be8a92063a8ae5829419f8d6
Basia Madej <barbara.madej@gmail.com>
2017-02-09 21:58:29 +0100

    Coaches: show all

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -2,6 +2,6 @@ class CoachesController < ApplicationController
 
   def index
     @title = t('coaches.headline')
-    @coaches = Coach.visible.regular.all.shuffle
+    @coaches = Coach.visible.all.shuffle
   end
 end
